4e1fe793f4bbb7e1f1248fa025fe9435173321ba
Pikachu <xuh013@ucsd.edu>
2022-10-22 01:25:48 -0700

    Part1 Working in progress

---
+++ expose/javascript/part1.md
@@ -0,0 +1,5 @@
+1. 20
+2. 20
+3. 20
+4. ___ReferenceError: result is not defined.___ Since the word ```let```has block scope, if it uses outside the block it is in, the program cannot refer to data anymore.
+5. TypeError: 
---
+++ expose/javascript/part1_test.js
@@ -0,0 +1,36 @@
+// // var declaration 
+// function sumValues(num1,num2,add) {
+//     if (add) {
+//         var result = 0;
+//         result = num1 + num2;
+//         console.log('values added: ', result);
+//     } else return;
+//     console.log('final result: ', result);
+// }
+
+// sumValues(10,10,true);
+
+
+// // let declaration
+// function sumValues(num1,num2,add) {
+//     if (add) {
+//         let result = 0;
+//         result = num1 + num2;
+//         console.log('values added: ', result);
+//     } else return;
+//     console.log('final result: ', result);
+// }
+
+// sumValues(10,10,true);
+
+// const declaratio
+function sumValues(num1,num2,add) {
+    if (add) {
+        const result = 0;
+        result = num1 + num2;
+        console.log('values added: ', result);
+    } else return;
+    console.log('final result: ', result);
+}
+
+sumValues(10,10,true);
